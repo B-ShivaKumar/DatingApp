26e44545ce06e04e3593f276134bbd1720214649
B-ShivaKumar <bejugam.shivakumar982@gmail.com>
2021-02-06 02:32:30 +0530

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,38 +1,47 @@
+using System.Collections;
+using System.Net.Mime;
+using System;
 using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using System.Collections.Generic;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using API.DTOs;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+
 namespace API.Controllers
 {
-    
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IMapper _mapper;
+        
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
+   
 
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-           return await  _context.Users.ToListAsync();
-            
+            var users = await _userRepository.GetMembersAsync();
+           return Ok(users);
         }
 
-        [Authorize]
-        [HttpGet("{id}")]
-        public async  Task<ActionResult<AppUser>> GetUser(int id)
+        
+        [HttpGet("{username}")]
+        public async  Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await  _context.Users.FindAsync(id);
-             
+             return await _userRepository.GetMemberAsync(username);
         }
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,9 +1,11 @@
-
+using System.Net.Security;
+using API.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -13,6 +15,8 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService,TokenService>();
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options=>{
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
